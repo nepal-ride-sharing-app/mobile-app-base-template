4311bb2f0d576e1383fcb8db5fba1c22ef442759
Subash Niroula <25361033+subash1999@users.noreply.github.com>
2025-01-04 20:51:21 +0900

    template updated

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,31 @@
-module.exports = {
-    presets: [
-        [
-            "@babel/preset-env",
-            {
-              "modules": false
-            }
-          ],
-        'module:metro-react-native-babel-preset',
-        '@babel/preset-react'  // Add this line
-    ],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          envName: 'APP_ENV',
-          moduleName: '@env',
-          path: `./env/.env.${process.env.APP_ENV || 'local'}`,  // Load the environment file dynamically
-          blocklist: null,
-          allowlist: null,
-          blacklist: null, // DEPRECATED
-          whitelist: null, // DEPRECATED
-          safe: false,
-          allowUndefined: true,
-          verbose: false,
-        },
-      ],
-    ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    // this is commented out because of conflict with expo
+    // refer to this thread on github, will use this after issue is fixed
+    // https://github.com/goatandsheep/react-native-dotenv/issues/501
+    // alternative solution proposed here
+    // https://github.com/cchoi94/react-native-dotenv
+    // the current solution uses expo default which is automatic refer to the following
+    // https://docs.expo.dev/guides/environment-variables/
+    // https://github.com/bkeepers/dotenv/blob/c6e583a/README.md#what-other-env-files-can-i-use
+    // plugins: [
+    //   [
+    //     "module:react-native-dotenv",
+    //     {
+    //       envName: "APP_ENV",
+    //       moduleName: "@env",
+    //       path: `./.env.${process.env.APP_ENV || "local"}`, // Load the environment file dynamically
+    //       blocklist: null,
+    //       allowlist: null,
+    //       blacklist: null, // DEPRECATED
+    //       whitelist: null, // DEPRECATED
+    //       safe: false,
+    //       allowUndefined: true,
+    //       verbose: false,
+    //     },
+    //   ],
+    // ],
   };
+};
--- types/env.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@env' {
-    export const APP_URL: string;
-    export const NODE_ENV: string;
-  }
